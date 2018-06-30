9bac9847f6e8b17487cd3556656de90f60e4a50e
LadyBug <40721933+MiraculousLadyBug@users.noreply.github.com>
2018-06-30 12:48:33 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,6 +8,10 @@ client.on('ready', () => {
 client.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
+        
+client.on('message', message => {
+    if (message.content === 'miraculous') {
+    	message.reply('LADYBUG!');
   	}
 });
 
